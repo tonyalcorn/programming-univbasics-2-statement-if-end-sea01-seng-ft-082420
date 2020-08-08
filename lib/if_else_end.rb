30c3359c7a116e564653f6e929df243b8bc0fda9
Anthony Alcorn <xmastony@gmail.com>
2020-08-08 07:10:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,1 @@
-if true
-  puts "This code runs!"
-end
-
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+current_time = current_time.to_i
