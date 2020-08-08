901ecbe74ce035aeac40e1d948da725741f4a22b
Anthony Alcorn <xmastony@gmail.com>
2020-08-08 07:04:41 +0000

    Automatically backed up by Learn

--- example_file.rb
+++ example_file.rb
@@ -1,8 +1,1 @@
-chance_of_rain = 0.2
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+Time.now.year
