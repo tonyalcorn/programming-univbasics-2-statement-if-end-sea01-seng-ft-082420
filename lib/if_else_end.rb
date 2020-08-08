db33d316c32063b539cd6cb7918aa27cf6670c62
Anthony Alcorn <xmastony@gmail.com>
2020-08-08 06:59:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
-# Write your solution here
+if true
+  puts "This code runs!"
+end
+
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
